a02e78377402b0bb526b917d86c669e7e2bb68ff
Rahul Bharati <rahul@rahul-pc.localdomain>
2018-10-03 20:50:50 +0530

    write to file

--- processorder.php
+++ processorder.php
@@ -1,83 +1,66 @@
+<?php
+// create short variable names
+$tireqty = (int) $_POST['tireqty'];
+$oilqty = (int) $_POST['oilqty'];
+$sparkqty = (int) $_POST['sparkqty'];
+$address = preg_replace('/\t|\R/', ' ', $_POST['address']);
+$document_root = $_SERVER['DOCUMENT_ROOT'];
+$date = date('H:i, jS F Y');
+?>
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>Bob’s Auto Parts - Order Results</title>
-    </head>
-    <body>
-        <h1>Bob's Auto Parts</h1>
-        <h2>Order Results</h2>
-        <?php
-            define('TIREPRICE', 100);
-            define('OILPRICE', 10);
-            define('SPARKPRICE', 4);
-            // create short variable names
-            $tireqty = $_POST['tireqty'];
-            $oilqty = $_POST['oilqty'];
-            $sparkqty = $_POST['sparkqty'];
-            $find = $_POST['find'];
-            
-            $totalqty = 0;
-            $totalqty = $tireqty + $oilqty + $sparkqty;
-            if ($totalqty == 0)
-            {
-                echo '<p style="color:red">';
-                echo 'You did not order anything on the previous page!';
-                echo '</p>';
-                exit;
-            }
-            else 
-            {
-                if ($tireqty > 0)
-                    echo htmlspecialchars($tireqty).' tires<br />';
-                if ($oilqty > 0)
-                    echo htmlspecialchars($oilqty).' bottles of oil<br />';
-                if ($sparkqty > 0)
-                    echo htmlspecialchars($sparkqty).' spark plugs<br />';
-            }
-
-            if ($tireqty < 10) 
-            {
-                $discount = 0;
-            } 
-            elseif (($tireqty >= 10) && ($tireqty <= 49)) 
-            {
-                $discount = 5;
-            } 
-            elseif (($tireqty >= 50) && ($tireqty <= 99)) 
-            {
-                $discount = 10;
-            } 
-            elseif ($tireqty >= 100) 
-            {
-                $discount = 15;
-            }
-            switch($find) {
-                case "a" :
-                echo "<p>Regular customer.</p>";
-                break;
-                case "b" :
-                echo "<p>Customer referred by TV advert.</p>";
-                break;
-                case "c" :
-                echo "<p>Customer referred by phone directory.</p>";
-                break;
-                case "d" :
-                echo "<p>Customer referred by word of mouth.</p>";
-                break;
-                default :
-                echo "<p>We do not know how this customer found us.</p>";
-                break;
-            }
-            echo "<p>Items ordered: ".$totalqty."<br />";
-            $totalamount = 0.00;
-            
-            $totalamount = $tireqty * TIREPRICE
-            + $oilqty * OILPRICE
-            + $sparkqty * SPARKPRICE;
-            echo "Subtotal: $".number_format($totalamount,2)."<br />";
-            $taxrate = 0.10; // local sales tax is 10%
-            $totalamount = $totalamount * (1 + $taxrate);
-            echo "Total including tax: $".number_format($totalamount,2)."</p>";
-        ?>
-        
-    </body>
+<head>
+<title>Bob's Auto Parts - Order Results</title>
+</head>
+<body>
+<h1>Bob's Auto Parts</h1>
+<h2>Order Results</h2>
+<?php
+echo "<p>Order processed at " . date('H:i, jS F Y') . "</p>";
+echo "<p>Your order is as follows: </p>";
+$totalqty = 0;
+$totalamount = 0.00;
+define('TIREPRICE', 100);
+define('OILPRICE', 10);
+define('SPARKPRICE', 4);
+$totalqty = $tireqty + $oilqty + $sparkqty;
+echo "<p>Items ordered: " . $totalqty . "<br />";
+if ($totalqty == 0) {
+    echo "You did not order anything on the previous page!<br />";
+} else {
+    if ($tireqty > 0) {
+        echo htmlspecialchars($tireqty) . ' tires<br />';
+    }
+    if ($oilqty > 0) {
+        echo htmlspecialchars($oilqty) . ' bottles of oil<br />';
+    }
+    if ($sparkqty > 0) {
+        echo htmlspecialchars($sparkqty) . ' spark plugs<br />';
+    }
+}
+$totalamount = $tireqty * TIREPRICE
+     + $oilqty * OILPRICE
+     + $sparkqty * SPARKPRICE;
+echo "Subtotal: $" . number_format($totalamount, 2) . "<br />";
+$taxrate = 0.10; // local sales tax is 10%
+$totalamount = $totalamount * (1 + $taxrate);
+echo "Total including tax: $" . number_format($totalamount, 2) . "</p>";
+echo "<p>Address to ship to is " . htmlspecialchars($address) . "</p>";
+$outputstring = $date . "\t" . $tireqty . " tires \t" . $oilqty . " oil\t"
+    . $sparkqty . " spark plugs\t\$" . $totalamount
+    . "\t" . $address . "\n";
+// open file for appending
+@$fp = fopen("$document_root/PHP/orders/orders.txt", 'ab');
+if (!$fp) {
+    echo "<p><strong> Your order could not be processed at this time.
+Please try again later.</strong></p>";
+    exit;
+}
+flock($fp, LOCK_EX);
+fwrite($fp, $outputstring, strlen($outputstring));
+flock($fp, LOCK_UN);
+fclose($fp);
+echo "<p>Order written.</p>";
+?>
+</body>
 </html>
